9880cb2dd382fd4be98ff9fcf4a3977d3f40093c
Victor <victorhenzzowp@gmail.com>
2024-09-09 19:04:08 -0300

    removing old pages

--- lib/core/navigation/app_router.dart
+++ lib/core/navigation/app_router.dart
@@ -1,19 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:web_deep_link/core/navigation/app_page_route.dart';
-import 'package:web_deep_link/modules/first_page.dart';
-import 'package:web_deep_link/modules/second_page.dart';
-import 'package:web_deep_link/modules/third_screen.dart';
 
 abstract class AppRouter {
   const AppRouter();
 
   static const String initialRoute = '/first-page';
 
-  static final Map<String, AppPageRoute> _routes = {
-    '/third-page': ThirdPageRoute(),
-    '/second-page': SecondPageRoute(),
-    '/first-page': FirstPageRoute(),
-  };
+  static final Map<String, AppPageRoute> _routes = {};
 
   static Route<dynamic>? onGenerateRoute(final RouteSettings settings) {
     return _routes[settings.name]!.buildRoute(settings.arguments);
